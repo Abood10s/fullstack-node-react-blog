5d4cdbeb70ef56a7aae96c45fc5ee74683040422
Abood10s <saafeenabood@gmail.com>
2023-09-02 21:32:00 +0300

    unused imports

--- frontend/src/redux/ApiCalls/AuthApiCalls.js
+++ frontend/src/redux/ApiCalls/AuthApiCalls.js
@@ -1,6 +1,5 @@
 import { request } from "../../utils/API";
 import { authActions } from "../Slices/AuthSlice";
-import { toast } from "react-toastify";
 
 // Login User
 export const loginUser = (user) => {
